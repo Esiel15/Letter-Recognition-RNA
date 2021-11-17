b769f80eb6a2195ee03540d44afd438e9e3489ad
katsushika <marcoderio850@gmail.com>
2021-11-16 21:37:20 -0600

    Pobl1 Gen4

--- exec.sh
+++ exec.sh
@@ -4,4 +4,4 @@ echo "[i] Compilando"
 javac -cp ".;lib\weka.jar" -d bin src/**/*.java src/*.java
 
 echo "[i] Ejecutando"
-java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 2 3
+java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 1 4
