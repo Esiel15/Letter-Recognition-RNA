b67c88b1524f9153be828f8128ea18a57e5c87c0
Fatake <pace_lozano@hotmail.com>
2021-11-18 15:27:46 -0600

    Pobacion 1 y 2 Generacion7

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "[i] Ejecutando"
-java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 1 6
+java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 1 7
--- exec2.sh
+++ exec2.sh
@@ -4,4 +4,4 @@ echo "[i] Compilando"
 javac -cp ".;lib\weka.jar" -d bin src/**/*.java src/*.java
 
 echo "[i] Ejecutando"
-java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 2 6
+java --add-opens=java.base/java.lang=ALL-UNNAMED -Xms2048M -Xmx8192M -cp ".;lib\weka.jar;bin" App 2 7
